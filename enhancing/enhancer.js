301480fa54d09aa9a6a4c3e2fd34ac493db78106
Mario Amaya <thefozcat@gmail.com>
2019-05-13 15:19:25 -0700

    created tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,11 +6,27 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  // item.enhancement === 20 ? 20 : item.enhancement += 1
+  if (item.enhancement < 20) {
+    item.enhancement += 1
+  } else {
+    item.enhancement = 20
+  }
+  return {...item};
 }
 
 function fail(item) {
-  return { ...item };
+  // item.enhancement > 16 ? item.enhancement -= 1 : item.enhancement
+  if (item.enhancement >= 15) {
+    if (item.enhancement > 16) {
+      item.enhancement -= 1;
+    }
+    item.durability -= 10;
+  } else {
+    item.durability -= 5;
+  }
+  // item.enhancement >= 15 ? item.durability -= 10 : item.durability -= 5
+  return {...item};
 }
 
 function repair(item) {
@@ -22,5 +38,10 @@ function repair(item) {
 }
 
 function get(item) {
+  if (item.enhancement === 0) {
+    name: item.name
+  } else {
+    item.name = `[+${item.enhancement}] ${item.name}`
+  }
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -7,8 +7,35 @@ const broadsword = {
     durability: 55
 }
 
+// Should increase enhancement by 1
+
+it('should increase enhancement by 1', () => {
+    expect(enhancer.succeed(broadsword)).toHaveProperty('enhancement', 11)
+    expect(enhancer.succeed({enhancement: 8})).toHaveProperty('enhancement', 9)
+})
+
+// // Should decrease enhancement and durability on fail
+
+it('should decrease enhancement by 1 if enhancement level is greater than 16', () => {
+    expect(enhancer.fail({enhancement: 11, durability: 55})).toEqual({enhancement: 11, durability: 50})
+})
+
+it('should decrease enhancement by 1 if enhancement level is greater than 16', () => {
+    expect(enhancer.fail({enhancement: 17, durability: 55})).toEqual({enhancement: 16, durability: 45})
+})
+
+it('should decrease durability by 10 if enhancement level is greater than or equal to 15', () => {
+    expect(enhancer.fail({enhancement: 15, durability: 55})).toEqual({enhancement: 15, durability: 45})
+})
+
 // Should restore durability to 100
 
 it('should restore durability to 100', () => {
     expect(enhancer.repair(broadsword)).toHaveProperty('durability', 100);
+})
+
+// Should add enhancement level to name
+
+it('should add enhancement level to name', () => {
+    expect(enhancer.get({name: 'Short Sword', enhancement: 12})).toHaveProperty('name', '[+12] Short Sword')
 })
